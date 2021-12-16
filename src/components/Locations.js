f6e5dfc1d50cd675818b4b94cff42568cf2102b6
Deborah Campos <631care@gmail.com>
2021-12-16 11:58:23 -0500

    locations.test pass

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -5,16 +5,65 @@ import React from "react"
 class Locations extends React.Component{
   constructor(){
     super()
+    this.state={
+        locationData: [],
+        showData: false,
+        buttonText: "Show Locations"
+    }
 
   }
 
-  render(){
-    return (
-      <div className="locations">
-        <div> <h1>Locations</h1></div>
+  componentDidMount=()=>{
+    fetch("https://ghibliapi.herokuapp.com/locations")
+    .then((res)=> res.json())
+    .then((locations)=>{
+      this.setState({
+          locationData: locations
+      });
+    });
+  }
+  handleLocationFetch=()=>{
+        this.setState({
+            showData: true,
+            buttonText: "Hide Locations"
+        });
 
-      </div>
-    )
+      console.log(this.state.locationData)
+  }
+
+    handleHideLocations=()=>{
+            this.setState({
+                showData: false
+            })
+    }
+
+  render(){
+
+        let renderedLocations = this.state.locationData.map((location)=>{
+            return(
+                <div className="locations">
+                    <li> <div >Name: {location.name} <br/> Climate: {location.climate} <br/> Terrain: {location.terrain}</div></li>
+                </div>
+            )
+        })
+
+        if(!this.state.showData){
+            return (
+                <div className="locations">
+                  <div> <h1>List of Locations</h1></div>
+                  <button onClick={this.handleLocationFetch}>Show Locations</button>
+                </div>
+              )
+        } else if(this.state.showData){
+            return (
+                <div className="locations">
+                  <div> <h1>List of Locations</h1></div>
+                  <button onClick={this.handleHideLocations}>{this.state.buttonText}</button>
+                    <div><ul>{renderedLocations}</ul></div>
+                </div>
+              )
+        }
+  
   }
 }
 
